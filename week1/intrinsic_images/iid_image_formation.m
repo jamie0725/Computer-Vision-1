c3d0371f8ee2f4c93b0f2df93dbd3532f25b8215
Zardinality <asxaqz2@gmail.com>
2019-02-17 17:43:48 +0800

    fix bugs

--- week1/intrinsic_images/iid_image_formation.m
+++ week1/intrinsic_images/iid_image_formation.m
@@ -2,14 +2,23 @@ albedo = imread('./ball_albedo.png');
 shading = imread('./ball_shading.png');
 original_img = imread('./ball.png');
 
+original_img = original_img./255;
+
 albedo = double(albedo)./255;
 shading = double(shading)./255;
 reconstructed_img = (albedo.*shading)*255;
 % max(shading(:))
 % max(albedo(:))
-% max(reconstructed_img(:))
-prediff = abs(double(original_img)./255-reconstructed_img);
+max(reconstructed_img(:))
+% max(original_img(:)./255)
+prediff = abs(double(original_img)-reconstructed_img);
 diff = max(prediff(:));
-fprintf('max difference between original image and reconstructed image %lf', diff)
-
-% imshow(albedo)
+fprintf('max difference between original image and reconstructed image %f', diff)
+subplot(2,2,1)
+imshow(albedo)
+subplot(2,2,2)
+imshow(shading)
+subplot(2,2,3)
+imshow(uint16(original_img)*255)
+subplot(2,2,4)
+imshow(uint16(reconstructed_img)*255)
