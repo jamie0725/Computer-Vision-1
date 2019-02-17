eaf330e2b885a1b12538edd7bc3f86d0750931f5
Vincent Wen <x.wen@student.tue.nl>
2019-02-17 15:55:20 +0100

    Nearly finished Part1.

--- week1/photometric/load_syn_images.m
+++ week1/photometric/load_syn_images.m
@@ -17,22 +17,21 @@ image_stack = 0;
 V = 0;
 Z = 0.5;
 
-i_img = 1;
 for i = 1:nfiles
 
     % read input image
-    im_org = imread(fullfile(image_dir, files(i).name));
-%     if channel == 1
-%         im = im(:, :, channel);
-%     end
+    im = imread(fullfile(image_dir, files(i).name));
+    im = im(:, :, channel);
     
-    if i_img == 1
-        % stack at third dimension
-        [h, w, ~] = size(im_org);
-        image_stack = zeros(h, w, nfiles * channel, 'uint8');
+    % stack at third dimension
+    if image_stack == 0
+        [h, w] = size(im);
         fprintf('Image size (HxW): %dx%d\n', h, w);
-        V = zeros(nfiles * channel, 3, 'double');
+        image_stack = zeros(h, w, nfiles, 'uint8');
+        V = zeros(nfiles, 3, 'double');
     end
+    
+    image_stack(:, :, i) = im;
     
     % read light direction from image name
     name = files(i).name(8:end);
@@ -40,14 +39,7 @@ for i = 1:nfiles
     X = str2double(name(1:m));
     n = strfind(name,'.png')-1;
     Y = str2double(name(m+2:n));
-    
-    for i_c = 1:channel
-        im = im_org(:, :, i_c);       
-        image_stack(:, :, i_img) = im;       
-        V(i_img, :) = [-X, Y, Z];
-        i_img = i_img + 1;
-    end   
-            
+    V(i, :) = [-X, Y, Z];
 end
 
 % normalization
@@ -59,4 +51,3 @@ normV = sqrt(sum(V.^2, 2));
 scriptV = bsxfun(@rdivide, V, normV);
 
 end
-
--- week1/photometric/photometric_stereo.m
+++ week1/photometric/photometric_stereo.m
@@ -6,7 +6,7 @@ disp('Part 1: Photometric Stereo')
 
 % obtain many images in a fixed view under different illumination
 disp('Loading images...')
-image_dir = './MonkeyColor/';   % TODO: get the path of the script
+image_dir = './SphereColor/';   % TODO: get the path of the script
 %image_ext = '*.png';
 
 if contains(image_dir, "Color")
@@ -15,13 +15,28 @@ else
     nc = 1;
 end
 
-[image_stack, scriptV] = load_syn_images(image_dir, nc);
-[h, w, n] = size(image_stack);
-fprintf('Finish loading %d images.\n\n', n);
+normals_sum = 0;
+for i_c = 1:nc
+    [image_stack, scriptV] = load_syn_images(image_dir, i_c);
+    if i_c == 1
+        [h, w, n] = size(image_stack);           
+        albedo = zeros(h, w, nc);
+    end
+    fprintf('Finish loading %d images.\n\n', n);
 
-% compute the surface gradient from the stack of imgs and light source mat
-disp('Computing surface albedo and normal map...')
-[albedo, normals] = estimate_alb_nrm(image_stack, scriptV);
+    % compute the surface gradient from the stack of imgs and light source mat
+    disp('Computing surface albedo and normal map...')
+    [albedo_tmp, normals_tmp] = estimate_alb_nrm(image_stack, scriptV);
+    albedo(:, :, i_c) = albedo_tmp;
+    normals_sum = normals_sum + normals_tmp;
+end
+normals_sum(isnan(normals_sum)) = 0;
+
+normals = normals_sum ./ nc;
+% norm_normals = (normals(:, :, 1).^2 + normals(:, :, 2).^2 + normals(:, :, 3).^2) .^0.5;
+% normals = normals ./ norm_normals;
+% albedo(isnan(albedo)) = 0;
+
 
 
 %% integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
@@ -33,7 +48,7 @@ SE(SE <= threshold) = NaN; % for good visualization
 fprintf('Number of outliers: %d\n\n', sum(sum(SE > threshold)));
 
 %% compute the surface height
-height_map = construct_surface( p, q );
+height_map = construct_surface( p, q, 'row' );
 
 %% Display
 show_results(albedo, normals, SE);
