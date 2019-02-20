c3ff5003dba853dc3b3fa4d5ca5f15c03d233b6c
Zardinality <asxaqz2@gmail.com>
2019-02-21 02:31:05 +0800

    add comments to part3

--- week1/intrinsic_images/iid_image_formation.m
+++ week1/intrinsic_images/iid_image_formation.m
@@ -1,16 +1,17 @@
+% read all three images
 albedo = imread('./ball_albedo.png');
 shading = imread('./ball_shading.png');
 original_img = imread('./ball.png');
-
+% normalize the img to be within 0-255
 original_img = original_img./255;
 
+% normalize the images to be within 0-1
 double_albedo = double(albedo)./255;
 double_shading = double(shading)./255;
+% multiply the albedo and shading together
 reconstructed_img = (double_albedo.*double_shading)*255;
-% max(shading(:))
-% max(albedo(:))
-max(reconstructed_img(:))
-% max(original_img(:)./255)
+
+% calculate the diff between original image and reconstructed image
 prediff = abs(im2double(original_img)-reconstructed_img);
 diff = max(prediff(:));
 fprintf('max difference between original image and reconstructed image %f', diff)
--- week1/intrinsic_images/recoloring.m
+++ week1/intrinsic_images/recoloring.m
@@ -1,23 +1,28 @@
+% read the images needed
 albedo = im2double(imread('./ball_albedo.png'));
 shading = im2double(imread('./ball_shading.png'));
 original_img = im2double(imread('./ball.png'));
 
+% separate three channels
 albedo_r = albedo(:,:,1); % Red channel
 albedo_g = albedo(:,:,2); % Green channel
 albedo_b = albedo(:,:,3); % Blue channel
-
+% find the true color of each channel
 true_r = max(albedo_r(:));
 true_g = max(albedo_g(:));
 true_b = max(albedo_b(:));
 
 [true_r, true_g, true_b] % true colors
 
+% find the mask of the object in image
 image_mask = double(albedo>0);
 
+% separate the masks
 r_c = image_mask(:, :, 1);
 g_c = image_mask(:, :, 2);
 b_c = image_mask(:, :, 3);
 
+% remove the red and blue channel
 green_img = cat(3, r_c*0, g_c*1, b_c*0).*shading;
 fig=figure(1)
 subplot(1,4,1)
