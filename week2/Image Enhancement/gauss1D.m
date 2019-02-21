44c5778915ab8fb6433fe486bb0ba9635c4199c7
Vincent Wen <x.wen@student.tue.nl>
2019-02-21 12:26:22 +0100

    Update Gaussian 1D and 2D.

--- week2/Image Enhancement/gauss1D.m
+++ week2/Image Enhancement/gauss1D.m
@@ -4,4 +4,7 @@ function G = gauss1D( sigma , kernel_size )
         error('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
     end
     %% solution
+    x = -floor(kernel_size / 2):floor(kernel_size / 2);
+    G = 1 / (sigma * sqrt(2 * pi)) * exp(- (x .^ 2) / (2 * sigma ^ 2)); 
+    G = G / sum(G);
 end
--- week2/Image Enhancement/gauss2D.m
+++ week2/Image Enhancement/gauss2D.m
@@ -1,3 +1,6 @@
 function G = gauss2D( sigma , kernel_size )
     %% solution
+    G_x = gauss1D(sigma, kernel_size);
+    G_y = gauss1D(sigma, kernel_size);
+    G = G_x' * G_y;
 end
