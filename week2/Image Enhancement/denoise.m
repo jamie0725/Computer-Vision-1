5b3348b972cdc6cb00e6c1564774a7ed3dfd2a43
GAO-Zhenyu <gaozhenyu@zju.edu.cn>
2019-02-24 15:19:08 +0100

    update

--- week2/Image Enhancement/denoise.m
+++ week2/Image Enhancement/denoise.m
@@ -2,10 +2,15 @@ function [ imOut ] = denoise( image, kernel_type, varargin)
 
 switch kernel_type
     case 'box'
-        fprintf('Not implemented\n')
+        %fprintf('Not implemented\n')
+        imOut = imboxfilt(image, varargin{1});
     case 'median'
-        fprintf('Not implemented\n')
+        %fprintf('Not implemented\n')
+        imOut = medfilt2(image, [varargin{1} varargin{2}]);
     case 'gaussian'
-        fprintf('Not implemented\n')
+        %fprintf('Not implemented\n')
+        h = gauss2D(varargin{1},varargin{2});
+        %imOut = imfilter(image, h);
+        imOut = conv2(h,im2double(image));
 end
 end
