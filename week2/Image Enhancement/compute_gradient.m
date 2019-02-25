e5ee0953900db70aeab792deec5290944fd42bfd
s2948044 <kai.liang@student.uva.nl>
2019-02-25 02:03:22 +0100

    update 4.3.1

--- week2/Image Enhancement/compute_gradient.m
+++ week2/Image Enhancement/compute_gradient.m
@@ -1,5 +1,6 @@
 function [Gx, Gy, im_magnitude,im_direction] = compute_gradient(image)
-fprintf('Not implemented\n')
-
+Gx = conv2(image, [1, 0, -1; 2, 0, -2; 1, 0, -1]);
+Gy = conv2(image, [1, 2, 1; 0, 0, 0; -1, -2, -1]);
+im_magnitude = sqrt(Gx.^2 + Gy.^2);
+im_direction = atan(Gy ./ Gx);
 end
-
---
+++ week2/Image Enhancement/main_sec4_3_1.m
@@ -0,0 +1,16 @@
+image = imread('./images/image2.jpg');
+[Gx, Gy, im_mag, im_dir] = compute_gradient(image);
+output = figure(1);
+subplot(2,2,1);
+imshow(Gx);
+title('Grad. of image in x-direction');
+subplot(2,2,2);
+imshow(Gy);
+title('Grad. of image in y-direction');
+subplot(2,2,3);
+imshow(im_mag);
+title('Grad. magnitude of each pixel');
+subplot(2,2,4);
+imshow(im_dir);
+title('Grad. direction of each pixel');
+saveas(output,'./images_4.3/gradient.eps','epsc');
