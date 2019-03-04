e20ef734ded923ddbc778dc0b354abb1c1e0e1c1
GAO-Zhenyu <gaozhenyu@zju.edu.cn>
2019-03-04 16:13:18 +0100

    update

--- week3/harris_corner_detector.m
+++ week3/harris_corner_detector.m
@@ -1,24 +1,22 @@
-function [H] = harris_corner_detector( image )
+function [H,r,c] = harris_corner_detector( image, threshold, N)
 %HARRIS_CORNER_DETECTOR1 Summary of this function goes here
 image_gray = rgb2gray(image);
 I = im2double(image_gray);
+height = size(I,1);
+width = size(I,2);
 
-
-size_image = size(image);
-H = zeros(size_image);
 
 sigma = 1;
-halfwid = sigma * 1;
+kernel_size = 3;
+kernel_radius = (kernel_size - 1)/2;
  
-[xx, yy] = meshgrid(-halfwid:halfwid, -halfwid:halfwid);
+[xx, yy] = meshgrid(-kernel_radius:kernel_radius, -kernel_radius:kernel_radius);
  
+%compute the first derivative of Gaussian filter
 Gx = xx .* exp(-(xx .^ 2 + yy .^ 2) / (2 * sigma ^ 2));
 Gy = yy .* exp(-(xx .^ 2 + yy .^ 2) / (2 * sigma ^ 2));
  
-%%apply sobel in herizontal direction and vertical direction compute the
-%%gradient
-%fx = [-1 0 1;-1 0 1;-1 0 1];
-%fy = [1 1 1;0 0 0;-1 -1 -1];
+%%compute the gradient
 Ix = conv2(I,Gx,'same');
 Iy = conv2(I,Gy,'same');
 %%compute Ix2, Iy2,Ixy
@@ -27,46 +25,40 @@ Iy2 = Iy.*Iy;
 Ixy = Ix.*Iy;
  
 %%apply gaussian filter
-%h = fspecial('gaussian',[6,6],1);
-
-h = gauss2D(1, 3);
+h = gauss2D(sigma, kernel_size);
 
 Ix2 = conv2(Ix2,h,'same');
 Iy2 = conv2(Iy2,h,'same');
 Ixy = conv2(Ixy,h,'same');
-height = size(I,1);
-width = size(I,2);
-result = zeros(height,width);
-R = zeros(height,width);
-Rmax = 0;
-%% compute M matrix and corner response
+
+%% compute H matrix and Q
+Q = zeros(height,width);
 for i = 1:height
     for j =1:width
-        M = [Ix2(i,j) Ixy(i,j);Ixy(i,j) Iy2(i,j)];
-        R(i,j) = det(M) - 0.04*(trace(M)^2);
-        if R(i,j)> Rmax
-            Rmax = R(i,j);
-        end
+        H = [Ix2(i,j) Ixy(i,j);Ixy(i,j) Iy2(i,j)];
+        Q(i,j) = det(H) - 0.04*(trace(H)^2);
     end
 end
-%% compare whith threshold
-count = 0;
-for i = 2:height-1
-    for j = 2:width-1
-        if R(i,j) > 0.01*Rmax
+
+%% If the point is a local maximum and compare whith threshold
+N_radius = (N - 1)/2;
+
+
+result = zeros(height,width);
+for i = 1+N_radius:height-N_radius
+    for j = 1+N_radius:width-N_radius
+        %compute the local maximum
+        Q_local = Q(i-N_radius:i+N_radius,j-N_radius:j+N_radius);
+        local_max = max(max(Q_local)); 
+        %judge the threshold
+        if Q(i,j) > threshold && Q(i,j) == local_max
             result(i,j) = 1;
-            count = count +1;
         end
     end
 end
  
-%non-maxima suppression
-result = imdilate(result, [1 1 1; 1 0 1; 1 1 1]);
  
-[posc,posr] = find(result == 1);
-imshow(I);
-hold on;
-plot(posr,posc,'r.')
+[c,r] = find(result == 1);
 end
 
 
--- week3/main_1.m
+++ week3/main_1.m
@@ -1,5 +1,15 @@
 image = imread('pingpong/0000.jpeg');
+figure(1);
+[H,r,c] = harris_corner_detector(image, 0.002, 3);
+imshow(image);
+hold on;
+plot(r,c,'r.')
 
-H = harris_corner_detector(image);
+figure(2);
+image = imread('person_toy/00000104.jpg');
+[H,r,c] = harris_corner_detector(image, 0.003, 3);
+imshow(image);
+hold on;
+plot(r,c,'r.')
 
 
