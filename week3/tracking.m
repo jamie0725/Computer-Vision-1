1ce5e2ec88eb52e2b002713413f1d92f377670d4
Zardinality <asxaqz2@gmail.com>
2019-03-06 20:05:39 +0800

    add quiver, still not working properly

--- week3/tracking.m
+++ week3/tracking.m
@@ -13,7 +13,7 @@ function tracking(base_dir, fmt, threshold, N)
     files = dir(fullfile(base_dir, sprintf('*.%s', fmt)));
     first_img_name = fullfile(base_dir, files(1).name);
     prev_img = imread(first_img_name);
-    [~,r,c] = harris_corner_detector(prev_img, threshold, N);
+    [~,r,c] = harris_corner_detector(prev_img, threshold, N, 'one');
     h = size(prev_img, 1);
     w = size(prev_img, 2);
     for i = 2:length(files)
@@ -25,12 +25,9 @@ function tracking(base_dir, fmt, threshold, N)
         imshow(prev_img);
         hold on
         s = scatter(r, c, 30);
+        hold on
         % set(s, 'Visible', 'off');
         s.MarkerEdgeColor = 'r';
-        drawnow;
-        % fr = getframe;
-        fr = getframe(fig);
-        writeVideo(videoObject, fr);
         new_img = imread(fullfile(base_dir, files(i).name));
         [Vx1, Vy1, ~] = lucas_kanade(prev_img, new_img, 'F');
         prev_img = new_img;
@@ -38,10 +35,19 @@ function tracking(base_dir, fmt, threshold, N)
         ind_y = min(ceil(c/winSize), size(Vx1, 1));
         v_r = Vx1(sub2ind(size(Vx1), ind_y, ind_x));
         v_c = Vy1(sub2ind(size(Vy1), ind_y, ind_x));
+        set(0, 'CurrentFigure', fig);
+        quiver(r, c, v_r, v_c);
+        hold on
         r = r+v_r*time_inteval;
         c = c+v_c*time_inteval;
         r = min(h, max(1, r));
-        c = min(size(prev_img, 2), max(1, c));
+        c = min(w, max(1, c));
+        % fr = getframe;
+        drawnow;
+        [~,name,~] = fileparts(files(i).name);
+        saveas(fig, fullfile('result_img', sprintf('%s.eps', name)), 'epsc');
+        fr = getframe(fig);
+        writeVideo(videoObject, fr);
         hold off
     end
     % filename = path_1(3:end-5); % adjust this to store the input image name if path is changed.
