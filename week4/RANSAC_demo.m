d33f957216b0b81478d699895546acfe990dfc87
s2948044 <kai.liang@student.uva.nl>
2019-03-11 22:32:37 +0100

    some updates

--- week4/RANSAC_demo.m
+++ week4/RANSAC_demo.m
@@ -7,11 +7,17 @@ I2 = im2single(imread('boat2.pgm'));
 
 T = affine2d([x(1) x(2) 0; x(3) x(4) 0; x(5) x(6) 1]);
 
-choice = "imwarp";
-if choice == "imwarp"
-    Image = imwarp(I2, T);
-else
-    Image = image_transform(I2, T);
-end
-
-imshow(im2uint8(Image));
+% image transform from boat 2 to boat 1.
+f1 = figure();
+subplot(1,2,1);
+% transform with MATLAB built-in function imwarp.
+out_image1 = imwarp(I2, T);
+imshow(im2uint8(out_image1));
+title('Transformation with built-in imwarp');
+subplot(1,2,2);
+% transform with our image_transform.m.
+out_image2 = image_transform(I2, T);
+imshow(im2uint8(out_image2));
+title('Transformation with image\_transform');
+sgtitle('Image transformation from boat2 to boat1');
+saveas(f1,'./results/boat2toboat1.eps','epsc');
--- week4/img_transform_demo.m
+++
@@ -1,7 +0,0 @@
-img1 = im2double(imread('./boat1.pgm'));
-
-% transform the image by $\frac{\pi}{4}$, and shift by [10, 20]
-T = affine2d([sqrt(2)/2 sqrt(2)/2 0; -sqrt(2)/2 sqrt(2)/2 0; 10 20 1]);
-
-result = image_transform(img1, T);
-imshow(result);
