0547ba4df3c434269892800d3a33106693728389
Zardinality <asxaqz2@gmail.com>
2019-03-12 03:55:23 +0800

    update the comments of img_transform

--- week4/img_transform_demo.m
+++ week4/img_transform_demo.m
@@ -1,14 +1,6 @@
 img1 = im2double(imread('./boat1.pgm'));
-% m = zeros(2,2);
-% m(1,1) = sqrt(2)/2;
-% m(1,2) = sqrt(2)/2;
-% m(2,1) = -sqrt(2)/2;
-% m(2,2) = sqrt(2)/2;
-% 
-% b = zeros(1,2);
-% b(1,1) = 10;
-% b(1,2) = 20;
 
+% transform the image by $\frac{\pi}{4}$, and shift by [10, 20]
 T = affine2d([sqrt(2)/2 sqrt(2)/2 0; -sqrt(2)/2 sqrt(2)/2 0; 10 20 1]);
 
 result = image_transform(img1, T);
