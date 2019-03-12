d84e301c982bb3cb53948ee360bd17b1d3742aac
GAO-Zhenyu <gaozhenyu@zju.edu.cn>
2019-03-12 20:23:49 +0100

    updata

--- week4/RANSAC.m
+++ week4/RANSAC.m
@@ -1,6 +1,10 @@
-function [x, points] = RANSAC(f1, f2, matches, N)
+function [x, matching_points] = RANSAC(f1, f2, matches, N)
+%% RANSAC function can get the affine matrix and matching points
+% There are 4 input arguements:closest matches in img2 to feature points in img1 
 % N is the number of loop
+%% sample_number is the number of sampling
 sample_number = 10;
+% record the number of max inliers
 max_inliers = 0;
 for i = 1:N
     a = randperm(size(matches, 2),sample_number);
@@ -15,19 +19,22 @@ for i = 1:N
         A(2*j-1:2*j,:) = [x1(j) y1(j) 0 0 1 0; 0 0 x1(j) y1(j) 0 1];
         b(2*j-1:2*j) = [x2(j); y2(j)];
     end
-
+    %compute the affine matrix
     temp_x = pinv(A) * b;
     
+    %compute the corresponding points after transform
     m = [temp_x(1) temp_x(2);temp_x(3) temp_x(4)];
     t = [temp_x(5);temp_x(6)];
     xy12 = m * [x1; y1] + t;
     x12 = xy12(1,:);
     y12 = xy12(2,:);
     
-    count = compute_lnliers(x2, y2, x12, y12);
-    if count>max_inliers
+    %judge if the transformed poins lied in the neighborhood
+    [count, transformed_matching_points] = compute_lnliers(x1, y1, x2, y2, x12, y12);
+    if count > max_inliers
         x = temp_x;
         max_inliers = count;
+        matching_points = transformed_matching_points;
     end
     
 end
@@ -35,13 +42,25 @@ end
 end
 
 %here we use the Euclidean distance
-function count = compute_lnliers(x2, y2, x12, y12)
+function [count, transformed_matching_points] = compute_lnliers(x1, y1, x2, y2, x12, y12)
 count = 0;
-length = size(x2);
+length = size(x2,2);
+radius = 10;
+
 for i = 1:length
-    if (((x12(i)-x2(i))^2+(y12(i)-y2(i))^2)<100)
+    if (((x12(i)-x2(i))^2+(y12(i)-y2(i))^2) < radius^2)
         count = count + 1;
-    end
+    end 
+end
+
+transformed_matching_points = zeros(4,count);
+j = 1;
+for i = 1:length
+    if (((x12(i)-x2(i))^2+(y12(i)-y2(i))^2) < radius^2)
+        c = [x2(i);y2(i);x12(i);y12(i)];
+        transformed_matching_points(:,j) = c;
+        j = j + 1;
+    end 
 end
 
 end
--- week4/RANSAC_demo.m
+++ week4/RANSAC_demo.m
@@ -1,14 +1,18 @@
 I1 = im2single(imread('boat1.pgm'));
 I2 = im2single(imread('boat2.pgm'));
 
+%I1 = im2single(rgb2gray(imread('left.jpg')));
+%I2 = im2single(rgb2gray(imread('right.jpg')));
+
 [f1, f2, matches, scores] = keypoint_matching(I1, I2);
 
-[x, ~] = RANSAC(f1, f2, matches, 11);
+[x, matching_points] = RANSAC(f1, f2, matches, 1000);
 
-T = affine2d([x(1) x(2) 0; x(3) x(4) 0; x(5) x(6) 1]);
+%T = affine2d([x(1) x(2) 0; x(3) x(4) 0; x(5) x(6) 1]);
+T = affine2d([x(1) x(3) 0; x(2) x(4) 0; -x(5) -x(6) 1]);
 
 % image transform from boat 2 to boat 1.
-f1 = figure();
+fig1 = figure();
 subplot(1,2,1);
 % transform with MATLAB built-in function imwarp.
 out_image1 = imwarp(I2, T);
@@ -19,5 +23,40 @@ subplot(1,2,2);
 out_image2 = image_transform(I2, T);
 imshow(im2uint8(out_image2));
 title('Transformation with image\_transform');
-sgtitle('Image transformation from boat2 to boat1');
-saveas(f1,'./results/boat2toboat1.eps','epsc');
+title('Image transformation from boat2 to boat1');
+saveas(fig1,'./results/boat2toboat1.eps','epsc');
+
+out_image1 = im2uint8(I2);
+out_image2 = im2uint8(image_transform(I1, T));
+size_image2 = size(out_image2);
+out_image1 = [out_image1 zeros(680,size_image2(2)-850)];
+out_image1 = [out_image1;zeros(size_image2(1)-680,size_image2(2))];
+
+fig2 = figure();
+Is = cat(4, out_image1,out_image2 );
+mnt = montage(Is, 'Size', [1 2]);
+title('Image transformation from boat2 to boat1');
+hold on
+p1_image = matching_points([1 2],:);
+p2_image = matching_points([3 4],:);
+% x_offset = size(out_image1, 2);
+% for i=1:size(matching_points,2)
+%     p1 = p1_image(:,i);
+%     p2 = p2_image(:,i);
+%     p2(1) = p2(1) + x_offset;
+%     p2(2) = p2(2);
+%     % Visualize keypoints and the connections between matches.
+%     plot(p1(1), p1(2), 'bo', 'LineWidth', 2.5, 'MarkerSize', 15)
+%     plot(p2(1), p2(2), 'rx', 'LineWidth', 2.5, 'MarkerSize', 15)
+%     line([p1(1), p2(1)], [p1(2), p2(2)], 'LineWidth', 2.5, 'color',rand(1,3))
+% end
+% hold off
+saveas(fig2,'./results/boat1_and_boat2_to_boat1.eps','epsc');
+
+%f = figure();
+%imshow(im2uint8(I1));
+%hold on;
+%out_image1 = imwarp(I2, T);
+%imshow(im2uint8(out_image1));
+%hold on;
+%imshow(im2uint8(I1))
