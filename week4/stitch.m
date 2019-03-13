f69644c1537e2303baf0a7a28943b917fe2dbafe
s2948044 <kai.liang@student.uva.nl>
2019-03-13 01:00:15 +0100

    some updates for stitch

--- week4/stitch.m
+++ week4/stitch.m
@@ -1,23 +1,17 @@
-function [T] = stitch(im_1, im_2)
+function [im_new] = stitch(im_1, im_2)
 %STITCH a function that takes an image pair as input, and return the stitched version.
 
-[f1, f2, matches, ~] = keypoint_matching(im_1, im_2);
-[x, points] = RANSAC(f1, f2, matches, 11);
-x1 = reshape(f1(1,points(1, :)), [], 1);
-y1 = reshape(f1(2,points(1, :)), [], 1);
-x2 = reshape(f2(1,points(2, :)), [], 1);
-y2 = reshape(f2(2,points(2, :)), [], 1);
-T = affine2d([x(1) x(2) 0; x(3) x(4) 0; x(5) x(6) 1]);
+[f1, f2, matches, ~] = keypoint_matching(im_2, im_1);
+[x, ~] = RANSAC(f1, f2, matches, 0.999);
+T = affine2d([x(1) x(3) 0; x(2) x(4) 0; -x(5) -x(6) 1]);
 transform_im2 = image_transform(im_2, T);
-[h, w] = size(im_1);
-l_r= w - des1(1,2) + des2(1,2);
-L = W + 1 - l_r
-R = W
-
-figure();
-subplot(1,2,1);
-imshow(im_1);
-subplot(1,2,2);
-imshow(transform_im2);
+[h_1, w_1] = size(im_1);
+[h_2, w_2] = size(transform_im2);
+t_1 = x(5);
+t_2 = x(6);
+H = max(h_1, h_2 + t_2) - 1;
+W = max(w_1, t_1 + w_2) - 1;
+im_new(t_2:H, t_1:W) = transform_im2;
+im_new(1:h_1, 1:w_1) = im_1;
 end
 
--- week4/stitch_demo.m
+++ week4/stitch_demo.m
@@ -5,4 +5,6 @@ im2 = imread('./right.jpg');
 % convert to single precision grey-scale images.
 pro1 = im2single(rgb2gray(im1));
 pro2 = im2single(rgb2gray(im2));
-[~] = stitch(pro1, pro2);
+[stitched] = stitch(pro1, pro2);
+figure();
+imshow(stitched);
