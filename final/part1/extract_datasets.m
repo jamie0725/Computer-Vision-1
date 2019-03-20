ed9c9c65242b67b12dd0e7510994b7c97ccc76ba
Vincent Wen <x.wen@student.tue.nl>
2019-03-20 14:05:25 +0100

    Small fix on dataset extraction.

--- final/part1/extract_datasets.m
+++ final/part1/extract_datasets.m
@@ -9,8 +9,8 @@ images = zeros(500 * 5, size(X, 2));
 labels = zeros(500 * 5, 1);
 for i_class = 1:size(classes,2)
     indices = find(tmp(:, size(X, 2) + 1) == classes(i_class));
-    images((i_class - 1) * 500 + 1: i_class * 500) = X(indices);
-    labels((i_class - 1) * 500 + 1: i_class * 500) = y(indices);
+    images((i_class - 1) * 500 + 1: i_class * 500, :) = X(indices, :);
+    labels((i_class - 1) * 500 + 1: i_class * 500, :) = y(indices, :);
 end
 
 save('train5.mat', 'images', 'labels', 'class_names');
@@ -24,8 +24,8 @@ images = zeros(800 * 5, size(X, 2));
 labels = zeros(800 * 5, 1);
 for i_class = 1:size(classes,2)
     indices = find(tmp(:, size(X, 2) + 1) == classes(i_class));
-    images((i_class - 1) * 800 + 1: i_class * 800) = X(indices);
-    labels((i_class - 1) * 800 + 1: i_class * 800) = y(indices);
+    images((i_class - 1) * 800 + 1: i_class * 800, :) = X(indices, :);
+    labels((i_class - 1) * 800 + 1: i_class * 800, :) = y(indices, :);
 end
 
 save('test5.mat', 'images', 'labels', 'class_names');
