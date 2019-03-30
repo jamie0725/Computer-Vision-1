9332f7a95a5c3b2cbd72b427c40c1c263fcaac43
Kai Liang <k.liang.2@student.rug.nl>
2019-03-30 16:54:31 +0100

    update

--- final/part1/feature_extractor.m
+++ final/part1/feature_extractor.m
@@ -14,33 +14,33 @@ O2Image = (RImage + GImage - 2 * BImage) / sqrt(6); % separate the O2 channel fo
 O3Image = (RImage + GImage + BImage) / sqrt(3); % separate the O3 channel for Opponent colorspace.
 
 if strcmp(sample_method, 'key')
-    [frames, descriptor] = vl_sift(greyImage); % SIFT key-point sampling for grey-scale image.
+    [frames, descriptor] = vl_sift(greyImage, 'PeakThresh', 1.5); % SIFT key-point sampling for grey-scale image.
     if strcmp(colorspace, 'rgb')
-        [~, Rdescriptor] = vl_sift(RImage, 'frames', frames); % SIFT key-point sampling for R channel.
-        [~, Gdescriptor] = vl_sift(GImage, 'frames', frames); % SIFT key-point sampling for G channel.
-        [~, Bdescriptor] = vl_sift(BImage, 'frames', frames); % SIFT key-point sampling for B channel.
+        [~, Rdescriptor] = vl_sift(RImage, 'Frames', frames); % SIFT key-point sampling for R channel.
+        [~, Gdescriptor] = vl_sift(GImage, 'Frames', frames); % SIFT key-point sampling for G channel.
+        [~, Bdescriptor] = vl_sift(BImage, 'Frames', frames); % SIFT key-point sampling for B channel.
         descriptor = cat(1, Rdescriptor, Gdescriptor, Bdescriptor);
 %         descriptor = reshape(descriptor, 128, 3, []);
     elseif strcmp(colorspace, 'orgb')
-        [~, O1descriptor] = vl_sift(O1Image, 'frames', frames); % SIFT key-point sampling for O1 channel.
-        [~, O2descriptor] = vl_sift(O2Image, 'frames', frames); % SIFT key-point sampling for O2 channel.
-        [~, O3descriptor] = vl_sift(O3Image, 'frames', frames); % SIFT key-point sampling for O3 channel.
+        [~, O1descriptor] = vl_sift(O1Image, 'Frames', frames); % SIFT key-point sampling for O1 channel.
+        [~, O2descriptor] = vl_sift(O2Image, 'Frames', frames); % SIFT key-point sampling for O2 channel.
+        [~, O3descriptor] = vl_sift(O3Image, 'Frames', frames); % SIFT key-point sampling for O3 channel.
         descriptor = cat(1, O1descriptor, O2descriptor, O3descriptor);
 %         descriptor = reshape(descriptor, 128, 3, []);
     end
 elseif strcmp(sample_method, 'dense')
     if strcmp(colorspace, 'grey')
-        [~, descriptor] = vl_dsift(greyImage); % SIFT dense sampling for grey-scale image.
+        [~, descriptor] = vl_dsift(greyImage, 'Step', 5); % SIFT dense sampling for grey-scale image.
     elseif strcmp(colorspace, 'rgb')
-        [~, Rdescriptor] = vl_dsift(RImage); % SIFT dense sampling for R channel.
-        [~, Gdescriptor] = vl_dsift(GImage); % SIFT dense sampling for G channel.
-        [~, Bdescriptor] = vl_dsift(BImage); % SIFT dense sampling for B channel.
+        [~, Rdescriptor] = vl_dsift(RImage, 'Step', 5); % SIFT dense sampling for R channel.
+        [~, Gdescriptor] = vl_dsift(GImage, 'Step', 5); % SIFT dense sampling for G channel.
+        [~, Bdescriptor] = vl_dsift(BImage, 'Step', 5); % SIFT dense sampling for B channel.
         descriptor = cat(1, Rdescriptor, Gdescriptor, Bdescriptor);
 %         descriptor = reshape(descriptor, 128, 3, []);
     elseif strcmp(colorspace, 'orgb')
-        [~, O1descriptor] = vl_dsift(O1Image); % SIFT dense sampling for O1 channel.
-        [~, O2descriptor] = vl_dsift(O2Image); % SIFT dense sampling for O2 channel.
-        [~, O3descriptor] = vl_dsift(O3Image); % SIFT dense sampling for O3 channel.
+        [~, O1descriptor] = vl_dsift(O1Image, 'Step', 5); % SIFT dense sampling for O1 channel.
+        [~, O2descriptor] = vl_dsift(O2Image, 'Step', 5); % SIFT dense sampling for O2 channel.
+        [~, O3descriptor] = vl_dsift(O3Image, 'Step', 5); % SIFT dense sampling for O3 channel.
         descriptor = cat(1, O1descriptor, O2descriptor, O3descriptor);
 %         descriptor = reshape(descriptor, 128, 3, []);
     end
