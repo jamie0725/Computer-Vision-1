da402a727d0efa83b08d0f7febdbbb4cf0a6e40f
Vincent Wen <x.wen@student.tue.nl>
2019-04-03 00:20:29 +0200

    Update main.m for part 4.2.

--- final/part2/main.m
+++ final/part2/main.m
@@ -8,10 +8,11 @@
 %% extract features and train svm
 
 % TODO: Replace the name with the name of your fine-tuned model
-nets.fine_tuned = load(fullfile(expdir, 'your_new_model.mat')); nets.fine_tuned = nets.fine_tuned.net;
+nets.fine_tuned = load(fullfile(expdir, 'net-epoch-120.mat')); nets.fine_tuned = nets.fine_tuned.net;
 nets.pre_trained = load(fullfile('data', 'pre_trained_model.mat')); nets.pre_trained = nets.pre_trained.net; 
 data = load(fullfile(expdir, 'imdb-stl.mat'));
 
 
 %%
+addpath liblinear-2.1
 train_svm(nets, data);
